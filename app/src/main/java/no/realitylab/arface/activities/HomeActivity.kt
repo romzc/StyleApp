b11211fe0cf2613b4b8f19fd413380cdabb851f5
rony <ronyramos495@gmail.com>
2023-07-20 23:48:51 -0500

    se termino el sidebar de la aplicacion, los items implementados son inicio y perfil de usuario

--- app/src/main/java/no/realitylab/arface/activities/HomeActivity.kt
+++ app/src/main/java/no/realitylab/arface/activities/HomeActivity.kt
@@ -1,18 +1,22 @@
 package no.realitylab.arface.activities
 
 import android.content.Intent
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.FrameLayout
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import kotlinx.android.synthetic.main.activity_home.bottomNavigationView
-import kotlinx.android.synthetic.main.activity_home.fabRv
+//import kotlinx.android.synthetic.main.activity_home.bottomNavigationView
+//import kotlinx.android.synthetic.main.activity_home.fabRv
 import no.realitylab.arface.R
 import no.realitylab.arface.ar_activities.GlassesActivity
 import no.realitylab.arface.ar_activities.MakeupActivity
@@ -21,16 +25,23 @@ import no.realitylab.arface.fragments.home.ProfileFragment
 import no.realitylab.arface.models.UserData
 import no.realitylab.arface.viewmodels.UserViewModel
 import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-    private lateinit var fabRv: FloatingActionButton
-    private lateinit var btmNavView: BottomNavigationView
+
     private lateinit var fragContainer: FrameLayout
     private lateinit var currentUser : UserData
 
-    val userVideModel : UserViewModel by viewModels()
+    private val userVideModel : UserViewModel by viewModels()
+
+    private lateinit var drawer : DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
     //val userViewModel: UserViewModel by viewModelFactory {  }
 
     //val userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
@@ -53,43 +64,81 @@ class HomeActivity : AppCompatActivity() {
         }
 
         userVideModel.updateUserModel(currentUser)
+
+
         initUI()
         initListeners()
     }
 
-
     private fun initUI() {
-        supportActionBar?.hide()
-        bottomNavigationView.menu.getItem(1).isEnabled = false
-        fabRv = findViewById(R.id.fabRv)
-        btmNavView = findViewById(R.id.bottomNavigationView)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+        toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open,R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
         fragContainer = findViewById(R.id.fragmentContainer)
         replaceFragment(HomeFragment())
+
     }
 
     private fun initListeners() {
-        fabRv.setOnClickListener {
-            startActivity(Intent(this, GlassesActivity::class.java))
-        }
 
-        btmNavView.setOnNavigationItemSelectedListener {menuItem ->  
-            when (menuItem.itemId) {
-                R.id.myProfile -> {
-                    replaceFragment(ProfileFragment())
-                    true
-                }
-                R.id.myHome -> {
-                    replaceFragment(HomeFragment())
-                    true
-                }
-                else -> false
-            }
-        }
     }
 
     private fun replaceFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fragmentContainer, fragment)
         transaction.commit()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when ( item.itemId ) {
+            R.id.nav_item_home -> {
+                replaceFragment(HomeFragment())
+            }
+            R.id.nav_item_profile-> {
+                replaceFragment(ProfileFragment())
+            }
+            R.id.nav_item_about -> {
+                Toast.makeText(this, "Item about", Toast.LENGTH_LONG).show()
+            }
+            R.id.nav_item_settings -> {
+                Toast.makeText(this, "Item settings", Toast.LENGTH_LONG).show()
+            }
+            R.id.nav_item_logout -> {
+                Toast.makeText(this, "Item logout", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
